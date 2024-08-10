544dd760df8e2a3bc2f127d8c4511898cd7aad21
yeju <yyejuu@naver.com>
2024-08-11 01:10:13 +0900

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+$(function () {
+	
+$('#pic').click(function(){
+    $(this).attr('src','/img/sub_bar_hover.png')
+    $('.thru .alba').css('display', 'block')
+})
+
+$('.thru .alba').click(function(){
+    $('.thru .alba .menu ').css('display', 'block')
+    $('.thru .alba .click_bubble').css('display', 'block')
+    $('.thru .alba .bubble').css('display', 'none')
+})
+});
